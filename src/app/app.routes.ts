886c30bcaea8485a4c51128990e60143fec4098d
Md Abrar Ur Rahman <ishmam000@gmail.com>
2024-11-05 18:43:45 +0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { ProductsComponent } from './Pages/products/products.component';
+import { AboutComponent } from './Pages/about/about.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { CartComponent } from './Pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",redirectTo:'home',pathMatch:'full'},
+    {path:'home',component:HomeComponent},
+    {path:'products',component:ProductsComponent},
+    {path:'about',component:AboutComponent},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'cart',component:CartComponent},
+];
